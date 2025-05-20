25dfcaa209a7ec90eb082573e84c13938877e217
Ram <rrivas@mail.umw.edu>
2025-05-20 08:05:30 -0400

    Understaning primitives, setting up functions to generate randomized primitives

--- W1/W1.cpp
+++ W1/W1.cpp
@@ -1,20 +1,99 @@
-// W1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <stdlib.h>
+#include <ctime>
 
-#include <iostream>
+//creates color based on randomly selected int values
+//returns a mapping of int values to form color
+ALLEGRO_COLOR makeColor();
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//draws a randomly colored pixel on a portion of the screen
+//@param color randomly generated color from makeColor() function
+//@param SCREEN_W the screen's width, an int
+//@param SCREEN_H the screen's height, an int
+void draw_random_pixel(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly colored and sized hollow ellipse on a portion of the screen
+void draw_random_ellipse(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly sized and color filled ellipse on a portion of the screen
+void draw_random_filled_ellipse(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly colored and sized line on a portion of the screen
+void draw_random_lines(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly colored and sized hollow rectangle on a portion of the screen
+void draw_random_rectangles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly sized and color filled rectangle on a portion of the screen
+void draw_random_filled_rectangles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly colored and sized hollow circle on a portion of the screen
+void draw_random_circles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly sized and color filled cicle on a portion of the screen
+void draw_random_filled_cirlces(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+//draws a randomly sized and color filled triangle on a portion of the screen
+void draw_random_filled_triangles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+
+int main(int argc, char* argv[]) {
+	int width = 800;
+	int height = 600;
+	srand(time(NULL));
+	ALLEGRO_DISPLAY* display = NULL;
+
+	if (!al_init()) {
+		fprintf(stderr, "Failed to initialize allegro!\n");
+		return -1;
+	}
+
+	display = al_create_display(width, height);
+	if (!display) {
+		fprintf(stderr, "Failed to create display!");
+		return -1;
+	}
+
+	al_flip_display();
+	al_rest(20.0);
+	al_destroy_display(display);
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+ALLEGRO_COLOR makeColor() {
+	int red = rand() % 255;
+	int green = rand() % 255;
+	int blue = rand() % 255;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	return (al_map_rgb(red, green, blue));
+}
+
+void draw_random_pixel(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_ellipse(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_filled_ellipse(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_lines(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_rectangles(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_filled_rectangles(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_circles(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_filled_circles(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
+void draw_random_triangles(ALLEGRO_COLOR color, const int width, const int height) {
+
+}
