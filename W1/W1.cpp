a4c993af66fd212896066de37052baddaee0be82
Ram <rrivas@mail.umw.edu>
2025-05-20 08:06:54 -0400

    Fixed spelling errors in functions

--- W1/W1.cpp
+++ W1/W1.cpp
@@ -33,7 +33,7 @@ void draw_random_filled_rectangles(ALLEGRO_COLOR color, const int SCREEN_W, cons
 void draw_random_circles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
 
 //draws a randomly sized and color filled cicle on a portion of the screen
-void draw_random_filled_cirlces(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
+void draw_random_filled_circles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
 
 //draws a randomly sized and color filled triangle on a portion of the screen
 void draw_random_filled_triangles(ALLEGRO_COLOR color, const int SCREEN_W, const int SCREEN_H);
@@ -94,6 +94,6 @@ void draw_random_circles(ALLEGRO_COLOR color, const int width, const int height)
 void draw_random_filled_circles(ALLEGRO_COLOR color, const int width, const int height) {
 
 }
-void draw_random_triangles(ALLEGRO_COLOR color, const int width, const int height) {
+void draw_random_filled_triangles(ALLEGRO_COLOR color, const int width, const int height) {
 
 }
